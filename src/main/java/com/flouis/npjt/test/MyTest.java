0788226a1bbeb7e1f6b4467172de61e9a8e7d216
flouis2018 <315587951@qq.com>
2018-09-18 17:27:52 +0800

    first commit

---
+++ src/main/java/com/flouis/npjt/test/MyTest.java
@@ -0,0 +1,10 @@
+package com.flouis.npjt.test;
+
+
+public class MyTest {
+
+    public static void main(String[] args) {
+        System.out.println("Current Thread:" + Thread.currentThread().getName());
+    }
+
+}
